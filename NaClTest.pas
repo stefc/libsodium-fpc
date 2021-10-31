61df75fb96a3fda03cf2d9c4653669cdc4c3dfe8
Stefc <stefan.boether@gmail.com>
2021-10-31 20:52:22 +0100

    Verschlüsselung implementiert  (XSalsa20)

--- NaClTest.pas
+++ NaClTest.pas
@@ -34,24 +34,50 @@ const
     $6f, $3b, $b1, $29, $26, $18, $b6, $fd, 
     $1c, $2f, $8b, $27, $ff, $88, $e0, $eb);
 
+  // Nonce ist 24 Bytes lang, wobei nur die ersten 16 Bytes in den HSalsa20 gegeben werden
+  noncePrefix : array[0..15] of Byte = (
+    $69, $69, $6e, $e9, $55, $b6, $2b, $73,
+    $cd, $62, $bd, $a8, $75, $fc, $73, $d6);
+
+
+  // Nonce ist 24 Bytes lang, wobei nur die ersten 16 Bytes in den HSalsa20 gegeben werden
+  nonceSuffix : array[0..7] of Byte = (
+    $82, $19, $e0, $03, $6b, $7a, $0b, $37);
+
 var 
   privKey, 
   pubAliceKey,
-  pubBobKey : TKey;
+  pubBobKey,
+  senderSharedSk,
+  receiverSharedSk,
+  shared,
+  key2 : TKey;
 
+  nonce : array[0..24-1] of Byte;
 
+  //buffer : array[0..4194304-1] of Byte; 
+  buffer : array[0..11-1] of Byte; 
+
+  crypted: array of Byte;
+
+  text: AnsiString;
 
 begin
+  // Step A: Create Private Key
   privKey := createPrivateKey();
   writeln('A random new private Key:');
+  writeln( ToHex( privKey ));
+  privKey := createPrivateKeyStd();
+  writeln('A new stand private Key:');
   writeln( ToHex( privKey ));
 
   writeln('Alice''s Priv Key:');
   writeln( ToHex( aliceSecret ));
   writeln('Bob''s Priv Key:');
-  writeln( ToHex( bobSecret )); 
+  writeln( ToHex( bobSecret ));
   writeln;
 
+  // Step B: Create Public Key
   pubAliceKey := createPublicKey(aliceSecret);
   writeln('Alice''s Pub Key:');
   writeln( ToHex( pubAliceKey ));
@@ -62,4 +88,40 @@ begin
   writeln( ToHex( pubBobKey ));
   // DE9EDB7D7B7DC1B4D35B61C2ECE435373F8343C85B78674DADFC7E146F882B4F
 
+  // Step C: Calculate the shared secret between Sender & Receiver
+  senderSharedSk := calcSharedSecret(aliceSecret, pubBobKey);
+  receiverSharedSk := calcSharedSecret(bobSecret, pubAliceKey);
+
+  writeln('Shared Secret (Alice to Bob)', Chr(9), ToHex(senderSharedSk));
+  writeln('Shared Secret (Bob from Alice)', Chr(9), ToHex(receiverSharedSk));
+
+  // Shared Secret between Bob's Public key and Alice's Private key
+  shared := senderSharedSk; 
+
+  key2 := encodeSharedSecret(shared, noncePrefix);
+  writeln('k2:' + toHex(key2));
+
+
+  Move(noncePrefix, nonce[0], Length(noncePrefix));
+  Move(nonceSuffix, nonce[16], Length(nonceSuffix));
+  writeln('Nonce:', toHex(nonce));
+  
+  // Verschlüsselung
+  FillChar(Buffer, Sizeof(Buffer), 0);
+  writeln('sha-256 (pre):' + toHex(hashSHA256(buffer)));
+  cryptXSalsa20(buffer, shared, nonce);
+  writeln('sha-256 (encode):' + toHex(hashSHA256(buffer)));
+
+  // Entschlüsselung
+  cryptXSalsa20(buffer, shared, nonce);
+  writeln('sha-256 (decode):' + toHex(hashSHA256(buffer)));
+
+  // Message verschlüsseln 
+
+  crypted := encodeXSalsa20('Hallo Welt!', shared, nonce);
+  writeln('Encrypted:' + toHex(crypted));
+
+  text := decodeXSalsa20(crypted, shared, nonce);
+  writeln('Decrypted:' + text);
+  
 end.
--- libSodium.pas
+++ libSodium.pas
@@ -7,9 +7,17 @@ interface
 
   type
     TKey = Array[0..KEY_SIZE-1] of Byte;
-
+    ByteArray = Array of Byte;
+    
   function createPublicKey(privKey: Array of Byte): TKey;
   function createPrivateKey(): TKey;
+  function createPrivateKeyStd(): TKey;
+  function calcSharedSecret(a: Array of Byte; b: Array of Byte): TKey;
+  function encodeSharedSecret(sharedSecret: array of Byte; nonceprefix: array of Byte): TKey;
+  function hashSHA256(data: array of Byte): TKey;
+  procedure cryptXSalsa20(var message: array of Byte; const sharedSecret: array of Byte; const nonce: Array of Byte); 
+  function encodeXSalsa20(const message: AnsiString; const sharedSecret, nonce: array of Byte): ByteArray; 
+  function decodeXSalsa20(const message: Array of Byte; const sharedSecret, nonce: array of Byte): AnsiString; 
 
 implementation
 
@@ -22,15 +30,82 @@ implementation
   procedure randombytes_buf(buf:PAnsiChar; const size: Integer); 
     cdecl; external;
 
+  function crypto_hash_sha256(output: PAnsiChar; const input: PAnsiChar; inlen: UInt64): Integer;
+    cdecl; external; 
+
+  function crypto_core_hsalsa20(output: PAnsiChar; const input: PAnsiChar; const k: PAnsiChar; const c: PAnsiChar): Integer;
+    cdecl; external; 
+
+  function crypto_stream_xsalsa20_xor(c: PAnsiChar; const m: PAnsiChar; mlen: UInt64; const n: PAnsiChar; const k: PAnsiChar): Integer;
+    cdecl; external;
 
   function createPublicKey(privKey: Array of Byte): TKey;
   begin
     crypto_scalarmult_curve25519_base(@Result, PAnsiChar(@privKey[0]));
   end;
 
+  function calcSharedSecret(a: Array of Byte; b: Array of Byte): TKey;
+  begin
+    Fillchar(Result, KEY_SIZE, 0);
+    crypto_scalarmult_curve25519(@Result, PAnsiChar(@a[0]), PAnsiChar(@b[0]));
+  end;
+  
   function createPrivateKey(): TKey;
   begin
     randombytes_buf(@Result,sizeof(TKey));
+  end;
+
+  function createPrivateKeyStd() : TKey;
+  begin
+    Fillchar(Result,KEY_SIZE,chr(42));
+    Result[0] := Result[0] and 248;
+    Result[31] := (Result[31] and 127) or 64;
+  end;
+
+  function calcHSalsa20(input: array of Byte; k: array of Byte; c: Array of Byte): TKey; 
+  begin
+    Fillchar(Result,KEY_SIZE,0);
+    crypto_core_hsalsa20(@Result, PAnsiChar(@input[0]), PAnsiChar(@k[0]), PAnsiChar(@c[0]));
+  end;
+
+  function encodeSharedSecret(sharedSecret: array of Byte; nonceprefix: array of Byte): TKey;
+  var 
+    s  : AnsiString = 'expand 32-byte k';
+    Zero, c, FirstKey : TKey;
+  begin    
+    Fillchar(Zero,KEY_SIZE,0);
+    Fillchar(c,KEY_SIZE,0);
+    Move(PAnsiChar(s)^, c, Length(s));
+    Firstkey := calcHSalsa20(Zero, sharedSecret, c);
+    Result := calcHSalsa20(nonceprefix, firstkey, c);
+  end;  
+
+  // SHA-256 Hashcode ermitteln
+  function hashSHA256(data: array of Byte): TKey;
+  begin
+    Fillchar(Result,KEY_SIZE,0);
+    crypto_hash_sha256(@Result, PAnsiChar(@data[0]), UInt64(Length(data)));
+  end;
+
+  // XSalsa20 Symetric Chiper
+  procedure cryptXSalsa20(var message: array of Byte; const sharedSecret: array of Byte; const nonce: Array of Byte); 
+  begin 
+    crypto_stream_xsalsa20_xor(@message, PAnsiChar(@message[0]), UInt64(Length(message)), 
+      PAnsiChar(@Nonce[0]), PAnsiChar(@sharedSecret[0]));
+  end;
+
+  function encodeXSalsa20( const message: AnsiString; const sharedSecret, nonce: array of Byte): ByteArray; 
+  begin
+    SetLength(Result, Length(message));
+    crypto_stream_xsalsa20_xor(@result[0], PAnsiChar(message), UInt64(Length(message)), 
+      PAnsiChar(@Nonce[0]), PAnsiChar(@sharedSecret[0]));
+  end;
+
+  function decodeXSalsa20(const message: Array of Byte; const sharedSecret, nonce: array of Byte): AnsiString; 
+  begin
+    SetLength(Result, Length(message));
+    crypto_stream_xsalsa20_xor(@result[1], PAnsiChar(@message[0]), UInt64(Length(message)), 
+      PAnsiChar(@Nonce[0]), PAnsiChar(@sharedSecret[0]));
   end;
 
 end.
